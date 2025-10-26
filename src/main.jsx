9dad7200a582f04182e097c929094873a28e3f90
Shadow <shadow@me>
2025-10-26 10:42:45 +0100

    Fix CSS import in main.jsx to use index.css

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Toaster } from 'react-hot-toast'
 import App from './App.jsx'
-import './styles/global.css'
+import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
