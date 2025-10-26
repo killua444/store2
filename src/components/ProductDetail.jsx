c26c0395820b83fe1bc8fe2cc5dea964ccc76736
Shadow <shadow@me>
2025-10-26 10:45:20 +0100

    Move JSON files to public folder and fix import paths

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -1,6 +1,6 @@
 import { useParams } from 'react-router-dom';
 import { useState } from 'react';
-import productsData from '../data/products.json';
+import productsData from '/products.json?url';
 import { waOrderLink, buildProductMessage } from '../utils/whatsapp';
 
 const ProductDetail = ({ addToCart, toggleWishlist, wishlist }) => {
